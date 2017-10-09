9aa1cad4f4558605afbf77a91c551293bb02e06b
Rony Xavier <rx294@nyu.edu>
2017-10-09 17:16:36 -0400

    new
    Signed-off-by: Rony Xavier <rx294@nyu.edu>

--- HW1/mapper.py
+++ HW1/mapper.py
@@ -3,20 +3,11 @@
 import sys
 import re
 
-# input comes from STDIN (standard input)
 for line in sys.stdin:
-    # remove leading and trailing whitespace
     line = line.strip()
 
-    # split the line into words
     words = re.findall(r"[\w']+", line)
-    # increase counters
     for x in range(0,len(words)-1):
-        # write the results to STDOUT (standard output);
-        # what we output here will be the input for the
-        # Reduce step, i.e. the input for reducer.py
-        #
-        # tab-delimited; the trivial word count is 1
         first = words[x]
         second = words[x + 1]
 
