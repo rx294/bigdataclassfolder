cbc4dc48e18a12e6b6bc184f27cc044a320cf696
Rony Xavier <rx294@nyu.edu>
2017-10-09 17:03:56 -0400

    new
    Signed-off-by: Rony Xavier <rx294@nyu.edu>

--- HW1/reducer.py
+++ HW1/reducer.py
@@ -3,37 +3,25 @@
 from operator import itemgetter
 import sys
 
-current_word = None
-current_count = 0
-word = None
+current_word_pair = None
+current_word_pair_count = 0
+word_pair = None
 
-# input comes from STDIN
 for line in sys.stdin:
-    # remove leading and trailing whitespace
     line = line.strip()
 
-    # parse the input we got from mapper.py
-    word, count = line.split('\t', 1)
-
-    # convert count (currently a string) to int
+    word_pair, count = line.split('\t', 1)
     try:
         count = int(count)
     except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
         continue
-
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
-        current_count += count
+    if current_word_pair == word_pair:
+        current_word_pair_count += count
     else:
-        if current_word:
-            # write result to STDOUT
-            print '%s\t%s' % (current_word, current_count)
-        current_count = count
-        current_word = word
+        if current_word_pair:
+            print '%s\t%s' % (current_word_pair, current_word_pair_count)
+        current_word_pair_count = count
+        current_word_pair = word
 
-# do not forget to output the last word if needed!
 if current_word == word:
-    print '%s\t%s' % (current_word, current_count)
+    print '%s\t%s' % (current_word_pair, current_word_pair_count)
