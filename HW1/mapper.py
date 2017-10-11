8122947e1d84bc02bff3e488dd003a83b86e18d2
Rony Xavier <rx294@nyu.edu>
2017-10-11 12:43:21 -0400

    new
    Signed-off-by: Rony Xavier <rx294@nyu.edu>

--- HW1/mapper.py
+++ HW1/mapper.py
@@ -7,6 +7,8 @@ for line in sys.stdin:
     line = line.strip()
 
     words = re.findall(r"[\w']+", line)
+    if len(words) == 1:
+        print '%s\t%s' % (words[0], 1)
     for x in range(0,len(words)-1):
         first = words[x]
         second = words[x + 1]
